f011ca1e3240b92490c0a827a8f034a6795988ce
minhtb51107 <binhminh51107@gmail.com>
2026-03-12 15:21:22 +0700

    sửa lại nội dung 2 file aplication

--- src/main/java/com/mindrevol/core/MindrevolBackendCoreApplication.java
+++ src/main/java/com/mindrevol/core/MindrevolBackendCoreApplication.java
@@ -2,6 +2,8 @@ package com.mindrevol.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MindrevolBackendCoreApplication {
@@ -9,5 +11,10 @@ public class MindrevolBackendCoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MindrevolBackendCoreApplication.class, args);
 	}
+	
+	 @Bean
+	    public RestTemplate restTemplate() {
+	        return new RestTemplate();
+	    }
 
 }
